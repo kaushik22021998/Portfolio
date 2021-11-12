f78ea968c19b6c693ef3f75bf82e3952f671083b
Kaushik Muthu Krishnan <46986350+kaushik22021998@users.noreply.github.com>
2021-11-12 17:55:37 +0530

    Update app_file.py

--- app_file.py
+++ app_file.py
@@ -17,19 +17,6 @@ def main():
     stocks = st.text_area(label = "Stocks")
     a=stocks.split(',')
     int_features = [str(x)+".NS" for x in a]
-    if st.checkbox("Equal Weight Portfolio"):
-        figs,returns,risks,risklevel,assets=Equal.figure(int_features)
-        buf = io.BytesIO()
-        figs.savefig(buf, format='png')
-        buf.seek(0)
-        pil_img = copy.deepcopy(Image.open(buf))
-        st.subheader('Weight Allocation')
-        st.image(pil_img)
-        st.subheader("Return and Risk of the Portfolio")
-        st.error("Volatility of the Portfolio: "+str(np.round(risks,2)))
-        st.error("Risk level is "+str(risklevel)) 
-        st.success("Expected Return of the Portfolio in Percentage: "+str(np.round(returns,2)))
-        buf.close()
     if st.checkbox("Optimal Risk Portfolio"):
         figs,returns,risks,risklevel,assets=Optimal.figure(int_features)
         buf = io.BytesIO()
